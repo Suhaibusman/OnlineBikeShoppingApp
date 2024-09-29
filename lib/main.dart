94d57225df954bdaeedea1dcf14e1ee01a010fdc
mehdinathani <mehdinathani@gmail.com>
2024-09-29 21:52:27 +0500

    update fav screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'OnLine Bike Shopping App',
+      title: 'Online Bike Shopping App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
